588d86f5675152402646f9a891d5155d25a83afa
Sidhant Panda <sidhantpanda@gmail.com>
2022-02-07 23:25:43 +0100

    ✨ Add basic fastify application

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('test');
+import Fastify from 'fastify'
+
+const fastify = Fastify({
+  logger: true
+})
+
+fastify.get('/', function (request, reply) {
+  reply.send({ hello: 'world' })
+})
+
+fastify.listen(3000, function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+});
